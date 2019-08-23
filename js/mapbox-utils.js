968769806d3c00d5ecf187698280acd132bdb5f7
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-08-23 16:55:31 -0500

    added bars object with 3 bars info

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -11,3 +11,27 @@ var map = new mapboxgl.Map({
 
 
 });
+
+
+
+// Bar Object
+var bars = [
+    {
+        name: "Jet-Setter",
+        address: "229 E Houston St #10, San Antonio, Tx 78205",
+        phone: "210-272-0457",
+        happyHour: "Blah Blah Blah"
+    },
+    {
+        name: "Chris Madrids San Antonio",
+        address: "600 N Presa St, San Antonio, Tx 78205",
+        phone: "210-267-9885",
+        happyHour: "Blah Blah Blah"
+    },
+    {
+        name: "Pinch Boil House",
+        address: "214 Broadway St, San Antonio, Tx 78205",
+        phone: "210-257-5152",
+        happyHour: "Blah Blah Blah"
+    }
+];
