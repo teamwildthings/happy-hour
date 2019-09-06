b9b9442b31151e749864ed6e3e55056a500ddf34
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-09-06 14:24:28 -0500

    self-locator

--- js/self-locator.js
+++ js/self-locator.js
@@ -1,11 +1,37 @@
 
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true
-}));
-
+// map.addControl(new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true
+//     },
+//     trackUserLocation: true
+// }));
+//
 $('#button').click(function () {
-    $('.mapboxgl-ctrl-icon .mapboxgl-ctrl-geolocate')
+navigator.geolocation.getCurrentPosition(position => {
+    const userCoordinates = [position.coords.longitude, position.coords.latitude];
+    map.addSource("user-coordinates", {
+        type: "geojson",
+        data: {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: userCoordinates
+            }
+        }
+    });
+    map.addLayer({
+        id: "user-coordinates",
+        source: "user-coordinates",
+        type: "circle"
+    });
+    map.flyTo({
+        center: userCoordinates,
+        zoom: 14
+    });
+    this.setState({
+        resolvingLocation: false,
+        userCoordinates
+    });
 });
+});
+
